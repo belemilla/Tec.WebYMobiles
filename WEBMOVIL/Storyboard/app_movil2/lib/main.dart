4574c0bfda84b64495b524f1275cac987e5646c0
belemilla <belenmillaray.munoz@alumnos.ulagos.cl>
2023-12-15 12:17:13 -0300

    actualizacion

--- WEBMOVIL/Storyboard/app_movil2/lib/main.dart
+++ WEBMOVIL/Storyboard/app_movil2/lib/main.dart
@@ -20,22 +20,6 @@ class _LoginPageState extends State<LoginPage> {
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
 
-  // Puedes usar Firebase para autenticación u otra librería según tus necesidades.
-  // Aquí se muestra un ejemplo con Firebase.
-  // Asegúrate de configurar Firebase en tu proyecto.
-
-  // Future<void> _signInWithEmailAndPassword() async {
-  //   try {
-  //     await FirebaseAuth.instance.signInWithEmailAndPassword(
-  //       email: _emailController.text,
-  //       password: _passwordController.text,
-  //     );
-  //     // Si el inicio de sesión es exitoso, puedes navegar a la siguiente pantalla
-  //   } catch (e) {
-  //     print('Error: $e');
-  //     // Manejar errores aquí
-  //   }
-  // }
 
   @override
   Widget build(BuildContext context) {
@@ -48,7 +32,7 @@ class _LoginPageState extends State<LoginPage> {
           crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
             Text(
-              'Partituras iep',
+              'Partituras IEP',
               style: TextStyle(
                 color: Colors.white,
                 fontSize: 24.0,
@@ -56,10 +40,8 @@ class _LoginPageState extends State<LoginPage> {
               ),
             ),
             SizedBox(height: 20.0),
-            Image.asset(
-              'assets/logo.jpg', // Añade la ruta de tu imagen
-              height: 50.0,
-            ),
+            Image.asset('assets/logo1.png', height: 50.0),
+
             SizedBox(height: 10.0),
             Text(
               'Bienvenido/a',
@@ -90,8 +72,7 @@ class _LoginPageState extends State<LoginPage> {
             SizedBox(height: 20.0),
             ElevatedButton(
               onPressed: () {
-                // Llama a la función de inicio de sesión aquí
-                //_signInWithEmailAndPassword();
+             
               },
               style: ElevatedButton.styleFrom(
                 primary: Colors.white,
@@ -106,7 +87,7 @@ class _LoginPageState extends State<LoginPage> {
             SizedBox(height: 10.0),
             TextButton(
               onPressed: () {
-                // Navega a la pantalla de registro aquí
+                
               },
               child: Text(
                 'Registrarse',
@@ -120,7 +101,7 @@ class _LoginPageState extends State<LoginPage> {
               alignment: Alignment.bottomLeft,
               child: TextButton(
                 onPressed: () {
-                  // Añade la lógica para "olvidaste tu contraseña" aquí
+                  
                 },
                 child: Text(
                   '¿Olvidaste tu contraseña?',

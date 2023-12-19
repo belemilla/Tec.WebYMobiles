15652cebe1a0092877352a34377e201b8b0f8533
belemilla <belenmillaray.munoz@alumnos.ulagos.cl>
2023-12-19 12:40:25 -0300

    actualizacion

--- WEBMOVIL/Storyboard/app_movil2/lib/main.dart
+++ WEBMOVIL/Storyboard/app_movil2/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -20,7 +21,6 @@ class _LoginPageState extends State<LoginPage> {
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
 
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -40,8 +40,7 @@ class _LoginPageState extends State<LoginPage> {
               ),
             ),
             SizedBox(height: 20.0),
-            Image.asset('assets/logo1.png', height: 50.0),
-
+            Image.asset('assets/images/logo1.png', height: 50.0),
             SizedBox(height: 10.0),
             Text(
               'Bienvenido/a',
@@ -51,6 +50,123 @@ class _LoginPageState extends State<LoginPage> {
               ),
             ),
             SizedBox(height: 20.0),
+            TextField(
+              key: Key('emailField'),
+              controller: _emailController,
+              decoration: InputDecoration(
+                hintText: 'Correo Electrónico',
+                filled: true,
+                fillColor: Colors.white,
+              ),
+            ),
+            SizedBox(height: 10.0),
+            TextField(
+              key: Key('passwordField'),
+              controller: _passwordController,
+              obscureText: true,
+              decoration: InputDecoration(
+                hintText: 'Contraseña',
+                filled: true,
+                fillColor: Colors.white,
+              ),
+            ),
+            SizedBox(height: 20.0),
+            ElevatedButton(
+              onPressed: () {
+                // Lógica para iniciar sesión con _emailController.text y _passwordController.text
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => HomePage()),
+                );
+              },
+              style: ElevatedButton.styleFrom(
+                primary: Colors.white,
+              ),
+              child: Text(
+                'Iniciar Sesión',
+                style: TextStyle(
+                  color: Colors.lightBlue,
+                ),
+              ),
+            ),
+            SizedBox(height: 10.0),
+            TextButton(
+              onPressed: () {
+                // Navegar a la página de registro
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => RegistrationPage()),
+                );
+              },
+              style: TextButton.styleFrom(
+                backgroundColor: Colors.transparent,
+                padding: EdgeInsets.zero,
+              ),
+              child: Text(
+                'Registrarse',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 16.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            SizedBox(height: 10.0),
+            Align(
+              alignment: Alignment.bottomLeft,
+              child: TextButton(
+                onPressed: () {
+                  // Lógica para recuperar contraseña
+                },
+                child: Text(
+                  '¿Olvidaste tu contraseña?',
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class RegistrationPage extends StatelessWidget {
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Registro'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Registro',
+              style: TextStyle(
+                fontSize: 24.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 20.0),
+            TextField(
+              controller: _nameController,
+              decoration: InputDecoration(
+                hintText: 'Nombre',
+                filled: true,
+                fillColor: Colors.white,
+              ),
+            ),
+            SizedBox(height: 10.0),
             TextField(
               controller: _emailController,
               decoration: InputDecoration(
@@ -72,44 +188,218 @@ class _LoginPageState extends State<LoginPage> {
             SizedBox(height: 20.0),
             ElevatedButton(
               onPressed: () {
-             
+                // Lógica para registrar al usuario con los datos ingresados
+                String name = _nameController.text;
+                String email = _emailController.text;
+                String password = _passwordController.text;
+
+                // Realizar las operaciones de registro aquí, como enviar los datos a un servidor
+                print('Nombre: $name, Correo Electrónico: $email, Contraseña: $password');
+                // Simulación de registro exitoso
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => HomePage()),
+                );
               },
               style: ElevatedButton.styleFrom(
-                primary: Colors.white,
+                primary: Colors.lightBlue,
               ),
               child: Text(
-                'Iniciar Sesión',
-                style: TextStyle(
-                  color: Colors.lightBlue,
-                ),
-              ),
-            ),
-            SizedBox(height: 10.0),
-            TextButton(
-              onPressed: () {
-                
-              },
-              child: Text(
-                'Registrarse',
+                'Registrar',
                 style: TextStyle(
                   color: Colors.white,
                 ),
               ),
             ),
-            SizedBox(height: 10.0),
-            Align(
-              alignment: Alignment.bottomLeft,
-              child: TextButton(
-                onPressed: () {
-                  
-                },
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  final List<String> pdfList = [
+    'Busca primero el reino de Dios.pdf',
+    'Desde el oriente azul.pdf',
+    'Despierta.pdf',
+    'Los presentes tiempos.pdf',
+    'Que grande es Dios.pdf',
+    'Te lo debo a ti.pdf',
+    'Tu eres el alfarero.pdf',
+    
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Inicio'),
+        actions: [
+          Center(
+            child: IconButton(
+              icon: Icon(Icons.search),
+              onPressed: () {
+                
+              },
+            ),
+          ),
+        ],
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: Color.fromARGB(255, 36, 167, 71),
+              ),
+              child: Center(
                 child: Text(
-                  '¿Olvidaste tu contraseña?',
+                  'Partituras',
                   style: TextStyle(
                     color: Colors.white,
+                    fontSize: 24,
                   ),
                 ),
               ),
+            ),
+            ListTile(
+              title: Text('Coro de Dorcas'),
+              onTap: () {
+                
+              },
+            ),
+            ListTile(
+              title: Text('Coro Adulto'),
+              onTap: () {
+                
+              },
+            ),
+            ListTile(
+              title: Text('Coro Juvenil'),
+              onTap: () {
+                
+              },
+            ),
+            ListTile(
+              title: Text('Coro Niños'),
+              onTap: () {
+                
+              },
+            ),
+          ],
+        ),
+      ),
+      body: Container(
+        decoration: BoxDecoration(
+          color: Colors.grey[200],
+        ),
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                'Índice de Partituras',
+                style: TextStyle(
+                  fontSize: 24.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            Expanded(
+              child: Container(
+                margin: EdgeInsets.all(8.0),
+                padding: EdgeInsets.all(8.0),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10.0),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.5),
+                      spreadRadius: 3,
+                      blurRadius: 7,
+                      offset: Offset(0, 3),
+                    ),
+                  ],
+                ),
+                child: ListView.builder(
+                  itemCount: pdfList.length,
+                  itemBuilder: (context, index) {
+                    return ListTile(
+                      title: Text(pdfList[index]),
+                      onTap: () {
+                        print('Abriendo el PDF: ${pdfList[index]}');
+                      },
+                    );
+                  },
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+            IconButton(
+              icon: Icon(Icons.account_circle),
+              onPressed: () {
+                
+              },
+            ),
+            IconButton(
+              icon: Icon(Icons.home),
+              onPressed: () {
+              
+              },
+            ),
+            IconButton(
+              icon: Icon(Icons.history),
+              onPressed: () {
+                
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class UserProfilePage extends StatelessWidget {
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Mi Cuenta'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Nombre del Usuario',
+              style: TextStyle(
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              'usuario@correo.com',
+              style: TextStyle(
+                fontSize: 16.0,
+              ),
+            ),
+            SizedBox(height: 20.0),
+            ElevatedButton(
+              onPressed: () {
+                
+                Navigator.pop(context); 
+              },
+              child: Text('Cerrar Sesión'),
             ),
           ],
         ),
